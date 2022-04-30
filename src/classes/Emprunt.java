e01a13359fbd54a547020b66708afb1060b3627b
Ayoub Ben Mahmoud <ayoub.benMahmoud@tek-up.de>
2022-04-30 01:33:08 +0100

    add-classes

--- src/classes/Emprunt.java
+++ src/classes/Emprunt.java
@@ -1,5 +1,9 @@
 package classes;
 
 public class Emprunt {
-
+	int numEmp ;
+	String dateEmp;
+	String dateRetour;
+	String refEmp;
+	String idEmp;
 }
--- src/classes/User.java
+++ src/classes/User.java
@@ -2,5 +2,4 @@ package classes;
 
 public class User {
 String name;
-
 }
