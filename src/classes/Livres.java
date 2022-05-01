6990c16a6b4fc1c1f457b4de743ea0b69517fdb6
ala-kad <kaddechiala@gmail.com>
2022-05-01 16:52:17 +0100

    add classes and gitignore

--- src/classes/Livres.java
+++ src/classes/Livres.java
@@ -1,6 +1,10 @@
 package classes;
 
 public class Livres extends Documents{
+	public Livres(String id, String t, String auth, boolean avail) {
+		super(id, t, auth, avail);
+		// TODO Auto-generated constructor stub
+	}
 	int nbrPages;
 	String author;
 }
--- src/classes/Magzines.java
+++ src/classes/Magzines.java
@@ -1,6 +1,11 @@
 package classes;
 
 public class Magzines extends Documents{
+	public Magzines(String id, String t, String auth, boolean avail) {
+		super(id, t, auth, avail);
+		// TODO Auto-generated constructor stub
+	}
+
 	String reference;
 	
 }
