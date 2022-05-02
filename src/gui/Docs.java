71623885ba68cd6ebbff8e998cf29de575adcadc
ala-kad <kaddechiala@gmail.com>
2022-05-02 18:20:37 +0100

    Update Docs.java

--- src/gui/Docs.java
+++ src/gui/Docs.java
@@ -1,13 +1,13 @@
 package gui;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
-import java.awt.Dimension;
+import javax.swing.JOptionPane;
 import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.RowSpec;
@@ -16,9 +16,16 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.sql.*;
+import DataBase.Queries;
+//import net.proteanit.sql.DbUtils;
+
 
 public class Docs extends JFrame {
-
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTextField textField_1;
@@ -69,6 +76,8 @@ public class Docs extends JFrame {
 				FormSpecs.RELATED_GAP_ROWSPEC,
 				FormSpecs.DEFAULT_ROWSPEC,
 				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
 				FormSpecs.DEFAULT_ROWSPEC,}));
 		
 		JLabel lblNewLabel = new JLabel("Book ID");
@@ -99,12 +108,51 @@ public class Docs extends JFrame {
 		contentPane.add(textField_3, "4, 8, fill, default");
 		textField_3.setColumns(10);
 		
-		JButton btnNewButton = new JButton("Submit");
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton submitBtn = new JButton("Submit");
+		submitBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		contentPane.add(btnNewButton, "4, 14, center, default");
-	}
-
+		contentPane.add(submitBtn, "4, 14, center, default");
+	//Testing Issued Books
+	JButton issued_but=new JButton("View Issued Books");//creating instance of JButton to view the issued books
+    issued_but.setBounds(280,20,160,25);//x axis, y axis, width, height 
+    issued_but.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e){
+                 
+                JFrame f = new JFrame("Users List");
+                //f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                 
+                 
+                Connection connection = null;
+				try {
+					connection = Queries.conn();
+				} catch (SQLException e2) {
+					// TODO Auto-generated catch block
+					e2.printStackTrace();
+				}
+                String sql="select * from Documents";
+                try {
+                    Statement stmt = connection.createStatement();
+                     stmt.executeUpdate("mediatheque");
+                    stmt=connection.createStatement();
+                    ResultSet rs=stmt.executeQuery(sql);
+                    JTable book_list= new JTable();
+   //                book_list.setModel(DbUtils.resultSetToTableModel(rs)); 
+                     
+                    JScrollPane scrollPane = new JScrollPane(book_list);
+ 
+                    f.getContentPane().add(scrollPane);
+                    f.setSize(800, 400);
+                    f.setVisible(true);
+                    f.setLocationRelativeTo(null);
+                } catch (SQLException e1) {
+                    // TODO Auto-generated catch block
+                     JOptionPane.showMessageDialog(null, e1);
+                }                                   
+    }
+        }
+    );
+     
+}
 }
