67398b843005d407ccd5aaa4c1da5c3b854eaa7a
ala-kad <kaddechiala@gmail.com>
2022-05-02 15:29:14 +0100

    Create interface Issue Books

--- src/gui/Docs.java
+++ src/gui/Docs.java
@@ -6,10 +6,24 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Dimension;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Docs extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
 
 	/**
 	 * Launch the application.
@@ -35,8 +49,62 @@ public class Docs extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new FormLayout(new ColumnSpec[] {
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),},
+			new RowSpec[] {
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,}));
+		
+		JLabel lblNewLabel = new JLabel("Book ID");
+		contentPane.add(lblNewLabel, "2, 2, right, default");
+		
+		textField = new JTextField();
+		contentPane.add(textField, "4, 2");
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("User ID");
+		contentPane.add(lblNewLabel_1, "2, 4, right, default");
+		
+		textField_1 = new JTextField();
+		contentPane.add(textField_1, "4, 4, fill, default");
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Period");
+		contentPane.add(lblNewLabel_2, "2, 6, right, default");
+		
+		textField_2 = new JTextField();
+		contentPane.add(textField_2, "4, 6, fill, default");
+		textField_2.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("Issued Day (DD-MM-YY)");
+		contentPane.add(lblNewLabel_3, "2, 8, right, default");
+		
+		textField_3 = new JTextField();
+		contentPane.add(textField_3, "4, 8, fill, default");
+		textField_3.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Submit");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		contentPane.add(btnNewButton, "4, 14, center, default");
 	}
 
 }
